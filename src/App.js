e26aa5f39840cd8441f82839e19e67222c08e05a
TheoLeao <theoleaoboudier@gmail.com>
2021-03-12 12:07:56 +0100

    replace the external leaftet css link with an internal link + trick to get the marker icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 
 import {
   BrowserRouter as Router,
@@ -11,11 +11,42 @@ import EditInformation from './components/pages/EditInformation'
 import TopNavigation from './components/TopNavigation';
 
 
+
 export default function App() {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [statusLocalization, setStatusLocalization] = useState(null);
+
+  
+  const getLocation = () => {
+    //check that the browser has the localisation function
+    if (!navigator.geolocation) {
+      setStatusLocalization('Geolocation is not supported by your browser');
+    } else {
+      setStatusLocalization('Locating...');
+      navigator.geolocation.getCurrentPosition((position) => {
+        setStatusLocalization(null);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, () => {
+        setStatusLocalization('Unable to retrieve your location');
+      });
+    }
+  }
+
   return (
     <Router>
       <div>
-       <TopNavigation></TopNavigation>
+        <TopNavigation></TopNavigation>
+        <div>
+          <button onClick={getLocation}>Get Location</button>
+          <h1>Coordinates</h1>
+          <p>{statusLocalization}</p>
+          {lat && <p>Latitude: {lat}</p>}
+          {lng && <p>Longitude: {lng}</p>}
+        </div>
+
+
         <Switch>
           <Route path="/edit-information">
             <EditInformation />
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import '../../css/home.css';
+import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 
 const Home = () => {
