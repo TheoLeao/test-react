2f1bd74125447ea2dc20d9e4228035854617c032
TheoLeao <theoleaoboudier@gmail.com>
2021-03-14 18:13:53 +0100

    update ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test react de Théo Boudier
+Nom: Boudier Théo
+Lien de l'application deployé: https://test-react-miw.herokuapp.com/
--- src/components/pages/ManageMarkers.jsx
+++ src/components/pages/ManageMarkers.jsx
@@ -15,7 +15,7 @@ const ManageMarkers = () => {
                     <div className="markerItem">
                         <div>Marker n° {marker.id}</div>
                         <div className="coords">Coordonnées: [{marker.coords[0]}, {marker.coords[1]}]</div>
-                        <div><button onClick={() => {dispatchMap({ type: "removeMarker", marker: { id: marker.id }})}}>Delete</button></div> 
+                        <div><button onClick={() => {dispatchMap({ type: "removeMarker", marker: { id: marker.id }})}}>Supprimer</button></div> 
                         <hr></hr>  
                     </div>
                 
